63b4e3246f6d8cc078d6384a1eb1951e9c92fb52
Alden Cutler <aldencutler84@gmail.com>
2024-09-01 22:39:58 -0400

    add main functionality: still need to add more styling and write README

--- public/js/main.js
+++ public/js/main.js
@@ -1,37 +1,201 @@
-// FRONT-END (CLIENT) JAVASCRIPT HERE
+/**
+ * Adds a student to the table.
+ * 
+ * @param {Object} student - The student object to be added.
+ * @returns {void}
+*/
+function addStudentToTable(student) {
+  let tbody = document.getElementById( 'table-body' )
+  let row = document.createElement( 'tr' )
+  let nameEl = document.createElement( 'td' )
+  let classYearEl = document.createElement( 'td' )
+  let gradeEl = document.createElement( 'td' )
+  let gradeLetterEl = document.createElement( 'td' )
 
-const submit = async function( event ) {
-  // stop form submission from trying to load
-  // a new .html page for displaying results...
-  // this was the original browser behavior and still
-  // remains to this day
-  event.preventDefault()
-  
+  nameEl.innerHTML = student.name
+  classYearEl.innerHTML = student.classYear
+  gradeEl.innerHTML = student.grade
+  gradeLetterEl.innerHTML = student.gradeLetter
+
+  row.appendChild( nameEl )
+  row.appendChild( classYearEl )
+  row.appendChild( gradeEl )
+  row.appendChild( gradeLetterEl )
+
+  tbody.appendChild( row )
+}
+
+/**
+ * Updates a student's entry in the table.
+ * 
+ * @param {Object} student - The student object to be updated.
+ * @returns {void}
+ */
+function updateStudentEntry(student) {
+  let table = document.getElementById('table-body');
+  let rows = table.getElementsByTagName('tr');
+  for (let i = 0; i < rows.length; i++) {
+    let name = rows[i].getElementsByTagName('td')[0].innerHTML;
+    if (name === student.name) {
+      rows[i].getElementsByTagName('td')[1].innerHTML = student.classYear;
+      rows[i].getElementsByTagName('td')[2].innerHTML = student.grade;
+      rows[i].getElementsByTagName('td')[3].innerHTML = student.gradeLetter;
+      return;
+    }
+  }
+
+  console.error("Student not found");
+}
+
+/**
+ * Deletes a student from the table.
+ * 
+ * @param {string} studentName - The name of the student to delete.
+ * @returns {void}
+ */
+function removeFromTable(studentName) {
+  let table = document.getElementById('table-body');  
+  let rows = table.getElementsByTagName('tr');
+  for (let i = 0; i < rows.length; i++) {
+    let name = rows[i].getElementsByTagName('td')[0].innerHTML;
+    if (name === studentName) {
+      table.deleteRow(i);
+      return;
+    }
+  }
+
+  console.error("Student not found");
+}
+
+function updateClassStats(stats) {
+  let allAvg = document.getElementById('all-avg'),
+      allCount = document.getElementById('all-count'),
+      freshmanAvg = document.getElementById('freshman-avg'),
+      freshmanCount = document.getElementById('freshman-count'),
+      sophomoreAvg = document.getElementById('sophomore-avg'),
+      sophomoreCount = document.getElementById('sophomore-count'),
+      juniorAvg = document.getElementById('junior-avg'),
+      juniorCount = document.getElementById('junior-count'),
+      seniorAvg = document.getElementById('senior-avg'),
+      seniorCount = document.getElementById('senior-count'),
+      gradAvg = document.getElementById('grad-avg'),
+      gradCount = document.getElementById('grad-count'),
+      partTimeAvg = document.getElementById('part-time-avg'),
+      partTimeCount = document.getElementById('part-time-count');
+
+  allAvg.innerHTML = stats.classAvg;
+  total = 0;
+  for (let key in stats.counts) {
+    total += stats.counts[key];
+  }
+  allCount.innerHTML = total;
+  freshmanAvg.innerHTML = stats.avgs.freshman;
+  freshmanCount.innerHTML = stats.counts.freshman;
+  sophomoreAvg.innerHTML = stats.avgs.sophomore;
+  sophomoreCount.innerHTML = stats.counts.sophomore;
+  juniorAvg.innerHTML = stats.avgs.junior;
+  juniorCount.innerHTML = stats.counts.junior;
+  seniorAvg.innerHTML = stats.avgs.senior;
+  seniorCount.innerHTML = stats.counts.senior;
+  gradAvg.innerHTML = stats.avgs.grad;
+  gradCount.innerHTML = stats.counts.grad;
+  partTimeAvg.innerHTML = stats.avgs.parttime;
+  partTimeCount.innerHTML = stats.counts.parttime;
+}
+
+/**
+ * Handles the form submission for adding a student.
+ * 
+ * @param {Event} e - The form submission event.
+ * @returns {void}
+ */
+const handleAdd = async function( e ) {
+  e.preventDefault()
+
+  // get all the input fields
   const name = document.querySelector( '#name' ),
-        message = document.querySelector( '#message' ),
-        json = { name: name.value, message: message.value },
+        classYear = document.querySelector( '#class' ),
+        grade = document.querySelector( '#grade' ),
+        json = { name: name.value, classYear: classYear.value, grade: grade.value },
         body = JSON.stringify( json )
 
-  if (name.value === "" || message.value === "") {
-    alert("Please fill out both fields.")
+  // form validation
+  if (name.value === "" || classYear.value === "" || grade.value === "") {
+    alert("Please fill out all fields.")
+    return
+  }
+  if (grade.value < 0 || grade.value > 100) {
+    alert("Please enter a valid grade.")
     return
   }
 
-  const response = await fetch( '/submit', {
+  // add the student to the table with their calculated grade letter
+  const response = await fetch( '/add', {
     method:'POST',
     body 
   })
 
   let res = await response.json()
-  let output = document.getElementById("response");
-  let li = document.createElement("li");
-  
   console.log(res)
-  li.textContent = `${res.name}, ${res.message}`;
-  output.appendChild(li);
+  if (response.status === 400) {
+    alert("Student already exists");
+    return;
+  }
+
+  let student = { name: name.value, classYear: classYear.value, grade: grade.value, gradeLetter: res.value }
+  if (response.status === 201) {
+    addStudentToTable(student)
+  } else if (response.status === 200) {
+    updateStudentEntry(student)
+  }
+  updateClassStats(res.stats);
 }
 
-window.onload = function() {
-   const button = document.querySelector("button");
-  button.onclick = submit;
+
+/**
+ * Handles the deletion of a student.
+ * 
+ * @param {Event} e - The form submission event.
+ * @returns {void}
+ */
+const handleDelete = async function( e ) {
+  e.preventDefault()
+  
+  const name = document.querySelector( '#name' ),
+        json = { name: name.value },
+        body = JSON.stringify( json )
+
+  if (name.value === "") {
+    alert("Please enter a name.")
+    return
+  }
+
+  const response = await fetch( '/delete', {
+    method:'POST',
+    body 
+  })
+  const res = await response.json()
+  
+  if (response.status === 400) {
+    alert("Student not found");
+    return;
+  } else if (response.status == 200) {
+    removeFromTable(name.value);
+    updateClassStats(res.stats);
+  }
+}
+
+window.onload = async function() {
+  const submitButton = document.getElementById("submit");
+  const deleteButton = document.getElementById("delete");
+  submitButton.onclick = handleAdd;
+  deleteButton.onclick = handleDelete;
+
+  // populate the table with the existing students (if any)
+  const response = await fetch( '/students', { method:'GET' } )
+  const res = await response.json()
+  if (res.students) {
+    res.students.forEach( student => addStudentToTable( student ) )
+  }
+  updateClassStats(res.stats);
 }
--- server.js
+++ server.js
@@ -1,3 +1,5 @@
+const { count } = require("console");
+
 const http = require("http"),
   fs = require("fs"),
   // IMPORTANT: you must run `npm install` in the directory for this assignment
@@ -7,8 +9,7 @@ const http = require("http"),
   mime = require("mime"),
   dir = "public/",
   port = 3000;
-
-const appdata = [];
+let students = [];
 
 const server = http.createServer(function (request, response) {
   if (request.method === "GET") {
@@ -23,28 +24,13 @@ const handleGet = function (request, response) {
 
   if (request.url === "/") {
     sendFile(response, "public/index.html");
+  } else if (request.url === "/students") {
+    // Added this to handle the GET request for the students list
+    response.writeHeader(200, { "Content-Type": "application/json" });
+    response.end(JSON.stringify({ students: students, stats: calculateClassGradeStats() }));
   } else {
     sendFile(response, filename);
   }
-};
-
-const handlePost = function (request, response) {
-  let dataString = "";
-
-  request.on("data", function (data) {
-    dataString += data;
-  });
-
-  request.on("end", function () {
-    let data = JSON.parse(dataString);
-
-    // ... do something with the data here!!!
-    appdata.push(data);
-    // console.log(appdata);
-
-    response.writeHead(200, "OK", { "Content-Type": "text/plain" });
-    response.end(dataString);
-  });
 };
 
 const sendFile = function (response, filename) {
@@ -63,5 +49,220 @@ const sendFile = function (response, filename) {
     }
   });
 };
+
+const handlePost = function (request, response) {
+  let dataString = "";
+
+  request.on("data", function (data) {
+    dataString += data;
+  });
+
+  request.on("end", function () {
+    let student = JSON.parse(dataString);
+
+    if (request.url === "/add") {
+      let code = addStudent(student);
+      if (code == 0) {
+        console.log("Student already exists");
+        response.writeHead(400, "Bad Request - Student already exists", {
+          "Content-Type": "text/plain",
+        });
+        response.end("400: Bad Request - Student already exists");
+      } else if (code == 1) {
+        console.log("Student added");
+        response.writeHead(201, "Created", {
+          "Content-Type": "application/json",
+        });
+        response.end(JSON.stringify({ value: student.gradeLetter, stats: calculateClassGradeStats() }));
+      } else if (code == 2) {
+        console.log("Student updated");
+        response.writeHead(200, "OK", { "Content-Type": "application/json" });
+        response.end(JSON.stringify({ value: student.gradeLetter, stats: calculateClassGradeStats() }));
+      }
+    } else if (request.url === "/delete") {
+      let success = deleteStudent(student);
+      if (success) {
+        response.writeHead(200, "OK", { "Content-Type": "application/json" });
+        response.end(JSON.stringify({ message: "Student deleted", stats: calculateClassGradeStats() }));
+      } else {
+        response.writeHead(400, "Bad Request - Student not found", {
+          "Content-Type": "text/plain",
+        });
+        response.end("400: Bad Request - Student not found");
+      }
+    }
+
+    console.log("students:");
+    console.log(students);
+  });
+};
+
+/**
+ * Adds a student to the list.
+ *
+ * @param {Object} student - The student object to be added.
+ * @returns {number} - The result code indicating the outcome of the operation:
+ *   - 0: Bad Request - Student already exists.
+ *   - 1: Student successfully added.
+ *   - 2: Student updated with different grade or class.
+ */
+function addStudent(student) {
+  // calculate the grade letter
+  let grade = student.grade;
+  switch (true) {
+    case grade >= 90:
+      grade = "A";
+      break;
+    case grade >= 80:
+      grade = "B";
+      break;
+    case grade >= 70:
+      grade = "C";
+      break;
+    default:
+      grade = "NR";
+  }
+
+  // check if the student is already in the list
+  for (let i = 0; i < students.length; i++) {
+    if (
+      students[i].name === student.name &&
+      students[i].classYear === student.classYear &&
+      students[i].grade === student.grade
+    ) {
+      return 0;
+    }
+    // if the student exists but has a different grade or class, update the other fields
+    else if (students[i].name === student.name) {
+      students[i].grade = student.grade;
+      students[i].classYear = student.classYear;
+      students[i].gradeLetter = grade;
+      student.gradeLetter = grade;
+      return 2;
+    }
+  }
+
+  // otherwise, if the student doesn't exist yet, add the student to the list
+  student.gradeLetter = grade;
+  students.push(student);
+  return 1;
+}
+
+/**
+ * Deletes a student from the list.
+ *
+ * @param {Object} student - The student object to be deleted.
+ * @returns {number} - The result code indicating the outcome of the operation:
+ *  - 0: Bad Request - Student not found.
+ *  - 1: Student successfully deleted.
+ */
+function deleteStudent(student) {
+  // check if the student is in the list
+  for (let i = 0; i < students.length; i++) {
+    if (students[i].name === student.name) {
+      students.splice(i, 1);
+      return 1;
+    }
+  }
+
+  // if the student is not in the list, return 0 to indicate that the student was not found
+  return 0;
+}
+
+/**
+ * Calculates the class grade statistics.
+ * 
+ * @returns {Object} - The class grade statistics object.
+ * - counts: The number of students in each class.
+ * - avgs: The average grade for each class.
+ * - classAvg: The average grade for the entire class.
+ * 
+ * Example:
+ * {
+ *  counts: {
+ *   senior: 2,
+ *  junior: 1,
+ * sophomore: 1,
+ * freshman: 1,
+ * grad: 1,
+ * parttime: 1
+ * },
+ * avgs: {
+ * senior: 85,
+ * junior: 90,
+ * sophomore: 80,
+ * freshman: 75,
+ * grad: 95,
+ * parttime: 85
+ * },
+ * classAvg: 85
+ * }
+ * 
+ * Note: The classAvg is the average of all the students' grades.
+ * */
+function calculateClassGradeStats() {
+  let counts = {
+    senior: 0,
+    junior: 0,
+    sophomore: 0,
+    freshman: 0,
+    grad: 0,
+    parttime: 0,
+  };
+  let avgs = {
+    senior: 0,
+    junior: 0,
+    sophomore: 0,
+    freshman: 0,
+    grad: 0,
+    parttime: 0,
+  };
+  let classAvg = 0;
+  for (let i = 0; i < students.length; i++) {
+    classAvg += parseInt(students[i].grade);
+    switch (students[i].classYear) {
+      case "senior":
+        counts.senior++;
+        avgs.senior += parseInt(students[i].grade);
+        break;
+      case "junior":
+        counts.junior++;
+        avgs.junior += parseInt(students[i].grade);
+        break;
+      case "sophomore":
+        counts.sophomore++;
+        avgs.sophomore += parseInt(students[i].grade);
+        break;
+      case "freshman":
+        counts.freshman++;
+        avgs.freshman += parseInt(students[i].grade);
+        break;
+      case "grad":
+        counts.grad++;
+        avgs.grad += parseInt(students[i].grade)
+        break;
+      case "part-time":
+        counts.parttime++;
+        avgs.parttime += parseInt(students[i].grade);
+        break;
+    }
+  }
+
+  classAvg = students.length == 0 ? 0 : parseFloat((classAvg / students.length).toFixed(2));
+  avgs.senior = counts.senior == 0 ? 0 : parseFloat((avgs.senior / counts.senior).toFixed(2));
+  avgs.junior = counts.junior == 0 ? 0 : parseFloat((avgs.junior / counts.junior).toFixed(2));
+  avgs.sophomore = counts.sophomore == 0 ? 0 : parseFloat((avgs.sophomore / counts.sophomore).toFixed(2));
+  avgs.freshman = counts.freshman == 0 ? 0 : parseFloat((avgs.freshman / counts.freshman).toFixed(2));
+  avgs.grad = counts.grad == 0 ? 0 : parseFloat((avgs.grad / counts.grad).toFixed(2));
+  avgs.parttime = counts.parttime == 0 ? 0 : parseFloat((avgs.parttime / counts.parttime).toFixed(2));
+
+  let classStats = {
+    counts: counts,
+    avgs: avgs,
+    classAvg: classAvg,
+  };
+
+  return classStats;
+}
 
 server.listen(process.env.PORT || port);

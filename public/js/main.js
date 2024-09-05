e02c306f1380e7892e46ff3b9e2dafcd429d57c5
Eddie Stump <ejstump13@gmail.com>
2024-09-05 15:33:46 -0400

    race track setup

--- public/js/main.js
+++ public/js/main.js
@@ -1,27 +1,45 @@
 // FRONT-END (CLIENT) JAVASCRIPT HERE
 
-const submit = async function( event ) {
+const submit = async function (event) {
   // stop form submission from trying to load
   // a new .html page for displaying results...
   // this was the original browser behavior and still
   // remains to this day
-  event.preventDefault()
-  
-  const input = document.querySelector( '#yourname' ),
-        json = { yourname: input.value },
-        body = JSON.stringify( json )
+  event.preventDefault();
 
-  const response = await fetch( '/submit', {
-    method:'POST',
-    body 
-  })
+  const input = document.querySelector("#yourname"),
+    json = { yourname: input.value },
+    body = JSON.stringify(json);
 
-  const text = await response.text()
+  const response = await fetch("/submit", {
+    method: "POST",
+    body,
+  });
 
-  console.log( 'text:', text )
-}
+  const text = await response.text();
 
-window.onload = function() {
-   const button = document.querySelector("button");
+  console.log("text:", text);
+};
+
+window.onload = function () {
+  const button = document.querySelector("button");
   button.onclick = submit;
-}
+};
+
+// Set up race track
+const canvas = document.getElementById("canvas");
+const ctx = canvas.getContext("2d");
+
+const size = window.innerHeight;
+canvas.width = size;
+canvas.height = size;
+
+ctx.fillStyle = "#2ab50b";
+ctx.fillRect(0, 0, size, size);
+
+const racetrack = document.getElementById("racetrack");
+ctx.drawImage(racetrack, 0, 0, 800, 800);
+
+// Draw racecar
+const racecar = document.getElementById("racecar");
+ctx.drawImage(racecar, 260, 45, 100, 50);

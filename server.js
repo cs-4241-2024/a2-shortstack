8b3626b80f01a8e2b09d16320260e336680d8035
Vivek Jagadeesh <107503192+vivekjag1@users.noreply.github.com>
2024-09-07 14:06:10 -0400

    fix problem with send file casuing a 404 error for all apis

--- server.js
+++ server.js
@@ -65,10 +65,19 @@ const handleGet = function( request, response ) {
 
   if( request.url === '/' ) {
     sendFile( response, 'public/index.html' )
-  }else{
+  }
+  else if(request.url === "/api/getResults"){
+    response.writeHead(200, "OK", {"content-type": 'application/json'}); 
+    response.end(JSON.stringify(purchases)); 
+    }
+ 
+  
+  
+  else{
     sendFile( response, filename )
   }
 }
+
 
 const handlePost = function( request, response ) {
   let dataString = ''
@@ -116,3 +125,56 @@ const sendFile = function( response, filename ) {
 }
 
 server.listen( process.env.PORT || port )
+/* 
+
+
+
+const handlePost = function( request, response ) {
+  let dataString = ''
+
+  request.on( 'data', function( data ) {
+      dataString += data 
+  })
+
+  request.on( 'end', function() {
+    console.log( JSON.parse( dataString ) )
+    const body = JSON.parse(dataString); 
+
+    if(request.url === "/api/createPurchase"){
+     
+      
+
+      addPurchase(body); 
+    }
+
+
+    response.writeHead( 200, "OK", {'Content-Type': 'text/plain' })
+    response.end('test')
+  })
+}
+
+const sendFile = function( response, filename ) {
+   const type = mime.getType( filename ) 
+
+   fs.readFile( filename, function( err, content ) {
+
+     // if the error = null, then we've loaded the file successfully
+     if( err === null ) {
+
+       // status code: https://httpstatuses.com
+       response.writeHeader( 200, { 'Content-Type': type })
+       response.end( content )
+
+     }else{
+
+       // file not found, error code 404
+       response.writeHeader( 404 )
+       response.end( '404 Error: File Not Found' )
+
+     }
+   })
+}
+
+server.listen( process.env.PORT || port )
+
+*/ 

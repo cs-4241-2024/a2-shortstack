f72ced33450df5a481468b30a1c4f81f25b2a92d
SupermanBritt <57573715+SupermanBritt@users.noreply.github.com>
2024-09-08 13:13:32 -0400

    Update main.js

--- public/js/main.js
+++ public/js/main.js
@@ -19,7 +19,6 @@ const submit = async function (event) {
 
   const response = await fetch('/submit', {
     method: 'POST',
-    // headers: { 'Content-Type': 'application/json' },
     body: JSON.stringify(newData)
   });
 

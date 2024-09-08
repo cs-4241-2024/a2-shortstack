ce2038fd6ca4b88db0ba1f4711c4a55fb1161a38
CraterMore <ctm6704@yahoo.com>
2024-09-08 17:06:29 -0400

    Finished project besides README

--- server.js
+++ server.js
@@ -1,3 +1,5 @@
+const { randomUUID } = require('crypto')
+
 const http = require( 'http' ),
       fs   = require( 'fs' ),
       // IMPORTANT: you must run `npm install` in the directory for this assignment
@@ -9,16 +11,19 @@ const http = require( 'http' ),
       port = 3000
 
 const appdata = [
-  { 'model': 'toyota', 'year': 1999, 'mpg': 23 },
-  { 'model': 'honda', 'year': 2004, 'mpg': 30 },
-  { 'model': 'ford', 'year': 1987, 'mpg': 14} 
+  // Example : { 'book-id': '1', 'title': "Book 1", 'author': "Author 1", "pages": 100, "started": "", "finished": "", "status": "read" },
 ]
 
+// Make server that listens for requests
 const server = http.createServer( function( request,response ) {
   if( request.method === 'GET' ) {
     handleGet( request, response )    
   }else if( request.method === 'POST' ){
     handlePost( request, response ) 
+  }else if( request.method === 'DELETE' ){
+    handleDelete( request, response ) 
+  } else if( request.method === 'PUT' ){
+    handlePut( request, response ) 
   }
 })
 
@@ -32,6 +37,7 @@ const handleGet = function( request, response ) {
   }
 }
 
+// Handle POST requests (add new book)
 const handlePost = function( request, response ) {
   let dataString = ''
 
@@ -40,15 +46,95 @@ const handlePost = function( request, response ) {
   })
 
   request.on( 'end', function() {
-    console.log( JSON.parse( dataString ) )
+    const newBook = JSON.parse( dataString ); // Parse JSON data
 
-    // ... do something with the data here!!!
+    // Set derived field
+    if (newBook["status"] === "reading" && newBook["started"] === "") {
+      const today = new Date().toLocaleDateString("en-us", {year: "numeric", month: "2-digit", day: "2-digit"});
+      newBook["started"] = today;
+    }
+    newBook["book-id"] = randomUUID();  // Set unique ID
+    newBook["avg-pages"] = null;
+    try {
+      if (newBook["started"] !== "" && newBook["finished"] !== "") { 
+        const startDate = new Date(newBook["started"]);
+        const finishDate = new Date(newBook["finished"]);
+        const diffDays = Math.ceil((finishDate - startDate) / (1000 * 60 * 60 * 24));
+        newBook["avg-pages"] = Math.round(newBook["pages"] / diffDays);
+      }
+    } catch (error) {
+      console.log("Error calculating average pages per day: " + error);
+    }
+    appdata.push( newBook );  // Add to appdata object
 
-    response.writeHead( 200, "OK", {'Content-Type': 'text/plain' })
-    response.end('test')
+
+    response.writeHead( 200, "OK", {'Content-Type': 'application/json'})
+    response.end(JSON.stringify(appdata)) // Return updated book data
   })
 }
 
+// Handle PUT requests (update book attribute)
+const handlePut = function( request, response ) {
+  let dataString = ''
+
+  request.on( 'data', function( data ) {
+      dataString += data 
+  })
+
+  request.on( 'end', function() {
+    const bookData = JSON.parse( dataString ); // Parse JSON data
+
+    // TODO: Update started and finished date for status change
+    let success = false;
+    appdata.forEach(book => { 
+      if (book["book-id"] === bookData["book-id"]) {
+        book[bookData["key"]] = bookData["value"];  // Update book attribute
+        success = true;
+        if (bookData["key"] === "status") {
+          if (bookData["value"] === "reading") {
+            const today = new Date().toLocaleDateString("en-us", {year: "numeric", month: "2-digit", day: "2-digit"});
+            book["started"] = today;
+          } else if (bookData["value"] === "read") {
+            const today = new Date().toLocaleDateString("en-us", {year: "numeric", month: "2-digit", day: "2-digit"});
+            book["finished"] = today;
+          }
+        }
+      } 
+    });
+
+    if (!success) { 
+      response.writeHead( 404, "Book Not Found", {'Content-Type': 'text/plain'});
+    } else {
+      response.writeHead( 200, "OK", {'Content-Type': 'application/json'})
+    }
+    response.end(JSON.stringify(appdata)) // Return updated book data
+  })
+}
+
+// Handle DELETE requests (delete book)
+const handleDelete = function( request, response ) {
+  let dataString = ''
+
+  request.on( 'data', function( data ) {
+      dataString += data 
+  })
+
+  request.on( 'end', function() {
+    console.log('going to delete ' + dataString)
+  
+    // Find book in appdata
+    const bookIndex = appdata.findIndex(book => book["book-id"] === dataString);
+    if (bookIndex !== -1) {
+      appdata.splice(bookIndex, 1);  // Remove book from appdata
+      response.writeHead( 200, "OK", {'Content-Type': 'application/json'});
+    } else {
+      response.writeHead( 404, "Book Not Found", {'Content-Type': 'text/plain'});
+    }
+    response.end(JSON.stringify(appdata));
+  })
+}
+
+// Handle file requests
 const sendFile = function( response, filename ) {
    const type = mime.getType( filename ) 
 

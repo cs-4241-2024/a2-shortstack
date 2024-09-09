d45bc57d45968b4e4efd0871c96bc662fa51bfd5
piperoco23 <proconnell@wpi.edu>
2024-09-09 08:09:33 -0400

    server errors

--- server.js
+++ server.js
@@ -8,7 +8,7 @@ const http = require( 'http' ),
     dir  = 'public/',
     port = 3000
 
-let appdata =  [
+const appdata =  [
   { 'name': 'Piper', 'cookie':'chocolate chip', 'icecream': 'vanilla', 'other':'', 'cake':'vanilla cake'},
   { 'name': 'James', 'cookie':'oatmeal no raisin', 'icecream': 'chocolate', 'other':'', 'cake':'chocolate cake'},
   { 'name': 'Sky', 'cookie':'sugar', 'icecream': 'vanilla', 'other':'', 'cake':'vanilla cake'}
@@ -28,7 +28,6 @@ const handleGet = function( request, response ) {
   if( request.url === '/' ) {
     sendFile( response, 'public/index.html' )
   }else if (request.url === '/data'){
-    //response.writeHead( 200, "OK", {'Content-Type': 'text/plain' })
     response.end(JSON.stringify(appdata))
   }
   else{
@@ -48,7 +47,8 @@ const handlePost = function( request, response ) {
     //console.log( JSON.parse( dataString ) )
     const data = JSON.parse(dataString)
     const tempDict = {'name':data[0], 'cookie':data[1], 'icecream':data[2], 'other':data[3], 'cake':data[4]}
-    appdata = appdata.concat(tempDict)
+    //appdata = appdata.concat(tempDict)
+    appdata.push(tempDict)
 
     //const data = JSON.parse( dataString )
     // ... do something with the data here!!!

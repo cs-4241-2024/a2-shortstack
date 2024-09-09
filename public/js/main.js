cdc2ebc5a76aba7b9db7964d61723af7e0d54803
arayahremillard <arayahjade16@gmail.com>
2024-09-08 21:40:26 -0400

    1st commit

--- public/js/main.js
+++ public/js/main.js
@@ -7,21 +7,49 @@ const submit = async function( event ) {
   // remains to this day
   event.preventDefault()
   
-  const input = document.querySelector( '#yourname' ),
-        json = { yourname: input.value },
-        body = JSON.stringify( json )
+  //collecting the form input
+  const nameInput = document.querySelector( '#name' );
+  const debutInput = document.querySelector( '#debut' );
+  const colorInput = document.querySelector( '#color' );
 
+  const json = { 
+    name: nameInput.value,
+    debut: debutInput.value, //parse?? parseInt(debutInput.value)
+    color: colorInput.value 
+  };
+  
+  const body = JSON.stringify( json );
+
+  //sending req
   const response = await fetch( '/submit', {
     method:'POST',
-    body 
-  })
+    body, 
+    headers: {
+      'Content-Type': 'application/json'
+    }
+  });
 
-  const text = await response.text()
+  const data = await response.json()
 
-  console.log( 'text:', text )
+  const ul = document.querySelector('ul')
+  ul.innerHTML = ''
+
+  data.forEach(item => {
+    const li = document.createElement('li');
+    li.innerText = `Name: ${item.name}, Debut: ${item.debut}, Color: ${item.color}, Existence: ${item.existence}`;
+    ul.appendChild(li);
+  });
+
+  nameInput.value = '';
+  debutInput.value = '';
+  colorInput.value = '';
+};
+
+const deleteButton = async function( event ) {
+
 }
 
 window.onload = function() {
-   const button = document.querySelector("button");
+  const button = document.querySelector("button");
   button.onclick = submit;
-}
+};

cf87846ec23f61b795b4f006b9b158c1fbe658ef
Samruddhi <107632561+samnaik10@users.noreply.github.com>
2024-09-09 11:49:18 -0400

    Update main.js

--- public/js/main.js
+++ public/js/main.js
@@ -7,21 +7,76 @@ const submit = async function( event ) {
   // remains to this day
   event.preventDefault()
   
-  const input = document.querySelector( '#yourname' ),
-        json = { yourname: input.value },
+  const wo = document.querySelector( 'input[name="wout"]:checked' ),
+        dat = document.querySelector( "#date"),
+        st = document.querySelector( "#stime"),
+        et = document.querySelector( "#etime"),
+        form = document.querySelector("form"),
+        json = { workout: wo.value, date: dat.value, stime: st.value, etime: et.value},
         body = JSON.stringify( json )
+  
+  console.log( body )
 
   const response = await fetch( '/submit', {
     method:'POST',
     body 
   })
 
-  const text = await response.text()
-
-  console.log( 'text:', text )
+  const values = await response.json()
+  updateTable(values)
+  
+  // form.reset()
 }
 
-window.onload = function() {
-   const button = document.querySelector("button");
+function updateTable(values){
+  const tbody = document.querySelector('tbody')
+
+  tbody.innerHTML = ''
+  values.forEach(value => {
+    const [sh, sm] = value.stime.split(":"),
+          [eh, em] = value.etime.split(":"),
+          h = eh - sh,
+          m = em - sm
+    
+    const element = document.createElement('tr'),
+          del = document.createElement('button')
+    
+    del.innerText = 'Delete'
+    del.addEventListener("click", async function(){
+      const response = await fetch('/delete', {
+        method: "DELETE",
+        headers: {
+          'Content-Type': 'application/json'
+        },
+        body: JSON.stringify(value)
+      })
+      
+      window.onload()
+     
+    })
+    
+    element.innerHTML = `<td>${value.workout}</td>
+                       <td>${value.date}</td>
+                       <td>${value.stime}</td>
+                       <td>${value.etime}</td>
+                       <td>${h * 60 + m}</td>`
+    
+    const td = document.createElement('td').appendChild(del)
+    element.appendChild(td)
+    tbody.appendChild( element )
+  })
+  
+}
+
+
+window.onload = async function() {
+  
+  const response = await fetch( '/gets' )
+  
+  const values = await response.json()
+  
+  updateTable(values)
+  
+  const button = document.querySelector("button");
   button.onclick = submit;
 }

48d763a238cd465078786daba2cd64095a060cee
Glitch (a2-aprilkat) <none>
2024-09-09 07:33:36 +0000

    🛩️🚘 Updated with Glitch

--- public/js/main.js
+++ public/js/main.js
@@ -1,27 +1,51 @@
 // FRONT-END (CLIENT) JAVASCRIPT HERE
 
-const submit = async function( event ) {
+const submit = async function (event) {
   // stop form submission from trying to load
   // a new .html page for displaying results...
   // this was the original browser behavior and still
   // remains to this day
-  event.preventDefault()
+  event.preventDefault();
+
+  // Clear the form when submit is hit (with flicker)
+  const form = document.querySelector("#tasklist");
+  form.innerHTML = "";
   
-  const input = document.querySelector( '#yourname' ),
-        json = { yourname: input.value },
-        body = JSON.stringify( json )
+  const date = new Date();
 
-  const response = await fetch( '/submit', {
-    method:'POST',
-    body 
-  })
+  const input = document.querySelector("#newtask");
+  const input3 = document.querySelector("input[name='priority']:checked"),
+    json = {
+      task: input.value,
+      creationDate: date.getDay(),
+      priority: input3.value,
+    },
+    body = JSON.stringify(json);
 
-  const text = await response.text()
+  const response = await fetch("/submit", {
+    method: "POST",
+    body,
+  });
 
-  console.log( 'text:', text )
-}
+  const data = await response.json();
+  data
+    .map((item) => item)
+    .forEach((item) => {
+      const element = document.createElement("div");
+      element.innerHTML =
+        "<input type='checkbox' name='todos'/><label>" +
+        `${item.task}` +
+        " - Due: " +
+        `${item.creationDate}` +
+        "</label>";
+      form.appendChild(element);
+    });
 
-window.onload = function() {
-   const button = document.querySelector("button");
+ 
+};
+
+
+window.onload = function () {
+  const button = document.querySelector("button");
   button.onclick = submit;
-}
+};
--- server.js
+++ server.js
@@ -1,74 +1,119 @@
-const http = require( 'http' ),
-      fs   = require( 'fs' ),
-      // IMPORTANT: you must run `npm install` in the directory for this assignment
-      // to install the mime library if you're testing this on your local machine.
-      // However, Glitch will install it automatically by looking in your package.json
-      // file.
-      mime = require( 'mime' ),
-      dir  = 'public/',
-      port = 3000
+const http = require("http"),
+  fs = require("fs"),
+  // IMPORTANT: you must run `npm install` in the directory for this assignment
+  // to install the mime library if you're testing this on your local machine.
+  // However, Glitch will install it automatically by looking in your package.json
+  // file.
+  mime = require("mime"),
+  dir = "public/",
+  port = 3000;
 
-const appdata = [
-  { 'model': 'toyota', 'year': 1999, 'mpg': 23 },
-  { 'model': 'honda', 'year': 2004, 'mpg': 30 },
-  { 'model': 'ford', 'year': 1987, 'mpg': 14} 
-]
+const appdata = [];
 
-const server = http.createServer( function( request,response ) {
-  if( request.method === 'GET' ) {
-    handleGet( request, response )    
-  }else if( request.method === 'POST' ){
-    handlePost( request, response ) 
+const server = http.createServer(function (request, response) {
+  if (request.method === "GET") {
+    handleGet(request, response);
+  } else if (request.method === "POST") {
+    handlePost(request, response);
   }
-})
+});
 
-const handleGet = function( request, response ) {
-  const filename = dir + request.url.slice( 1 ) 
+const handleGet = function (request, response) {
+  const filename = dir + request.url.slice(1);
 
-  if( request.url === '/' ) {
-    sendFile( response, 'public/index.html' )
-  }else{
-    sendFile( response, filename )
+  if (request.url === "/") {
+    sendFile(response, "public/index.html");
+  } else {
+    sendFile(response, filename);
   }
-}
+};
 
-const handlePost = function( request, response ) {
-  let dataString = ''
+const handlePost = function (request, response) {
+  let dataString = "";
 
-  request.on( 'data', function( data ) {
-      dataString += data 
-  })
+  request.on("data", function (data) {
+    dataString += data;
+  });
 
-  request.on( 'end', function() {
-    console.log( JSON.parse( dataString ) )
+  request.on("end", function () {
+    // obj
+    const data = JSON.parse(dataString);
 
-    // ... do something with the data here!!!
+    // check priority
+    if (data.priority === "High") {
+      let newDate = data.creationDate + 1;
+      if (newDate === 7) {
+        newDate = 0;
+      }
 
-    response.writeHead( 200, "OK", {'Content-Type': 'text/plain' })
-    response.end('test')
-  })
-}
+      data.creationDate = newDate;
+    } else if (data.priority === "Medium") {
+      let i = 0;
+      for (i = 0; i < 3; i++) {
+        let newDate = data.creationDate + 1;
+        if (newDate === 7) {
+          newDate = 0;
+        }
+        data.creationDate = newDate;
+      }
+    } else if (data.priority === "Low") {
+      let j = 0;
+      for (j = 0; j < 7; j++) {
+        let newDate = data.creationDate + 1;
+        if (newDate === 7) {
+          newDate = 0;
+        }
+        data.creationDate = newDate;
+      }
+    }
 
-const sendFile = function( response, filename ) {
-   const type = mime.getType( filename ) 
+    switch (data.creationDate) {
+      case 0:
+        data.creationDate = "Sunday";
+        break;
+      case 1:
+        data.creationDate = "Monday";
+        break;
+      case 2:
+        data.creationDate = "Tuesday";
+        break;
+      case 3:
+        data.creationDate = "Wednesday";
+        break;
+      case 4:
+        data.creationDate = "Thursday";
+        break;
+      case 5:
+        data.creationDate = "Friday";
+        break;
+      case 6:
+        data.creationDate = "Saturday";
+    }
 
-   fs.readFile( filename, function( err, content ) {
+    appdata.push(data);
 
-     // if the error = null, then we've loaded the file successfully
-     if( err === null ) {
+    console.log(JSON.parse(dataString));
 
-       // status code: https://httpstatuses.com
-       response.writeHeader( 200, { 'Content-Type': type })
-       response.end( content )
+    response.writeHead(200, "OK", { "Content-Type": "text/plain" });
+    response.end(JSON.stringify(appdata));
+  });
+};
 
-     }else{
+const sendFile = function (response, filename) {
+  const type = mime.getType(filename);
 
-       // file not found, error code 404
-       response.writeHeader( 404 )
-       response.end( '404 Error: File Not Found' )
+  fs.readFile(filename, function (err, content) {
+    // if the error = null, then we've loaded the file successfully
+    if (err === null) {
+      // status code: https://httpstatuses.com
+      response.writeHeader(200, { "Content-Type": type });
+      response.end(content);
+    } else {
+      // file not found, error code 404
+      response.writeHeader(404);
+      response.end("404 Error: File Not Found");
+    }
+  });
+};
 
-     }
-   })
-}
-
-server.listen( process.env.PORT || port )
+server.listen(process.env.PORT || port);

4d59c410b8f4cdfcc9fd14fadda8ef9fec73417f
HenryHribar <hhribar21@gmail.com>
2024-09-09 11:54:49 -0400

    Commit everything

--- public/js/main.js
+++ public/js/main.js
@@ -1,4 +1,5 @@
 // FRONT-END (CLIENT) JAVASCRIPT HERE
+const operatorSymbols = [' + ', ' - ', ' * ', ' / ']
 
 const submit = async function( event ) {
   // stop form submission from trying to load
@@ -7,21 +8,34 @@ const submit = async function( event ) {
   // remains to this day
   event.preventDefault()
   
-  const input = document.querySelector( '#yourname' ),
-        json = { yourname: input.value },
-        body = JSON.stringify( json )
+  const firstvalue = document.querySelector( '#firstvalue' )
+  const secondvalue = document.querySelector( '#secondvalue' )
+  const operators = document.getElementsByName( 'operator' )
+  let operator = -1
+  let operatorSymbol = ''
+  for (i = 0; i < operators.length; i++){
+    if(operators[i].checked){
+      operator = operators[i].value
+      operatorSymbol = operatorSymbols[i]
+    }
+  }
+  json = {
+    firstvalue: firstvalue.value,
+    secondvalue: secondvalue.value,
+    operator: operator
+  }
+  body = JSON.stringify( json )
 
   const response = await fetch( '/submit', {
     method:'POST',
     body 
   })
 
-  const text = await response.text()
-
-  console.log( 'text:', text )
+  const result = await response.text()
+  document.getElementById( 'equation' ).innerHTML = firstvalue.value + operatorSymbol + secondvalue.value + ' = ' + result
 }
 
 window.onload = function() {
-   const button = document.querySelector("button");
+  const button = document.querySelector("button");
   button.onclick = submit;
 }

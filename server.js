60a361e47309bcbb8aba73f9caea9c887773478e
Anthony <atitcombe13@gmail.com>
2024-09-16 14:38:06 -0400

    fix git oauth

--- server.js
+++ server.js
@@ -60,21 +60,26 @@ app.get('/main.html', (req, res) => {
   res.redirect('/');
 });
 
-app.get('/login', (req, res) => {
-  const client_id = process.env.CLIENT_ID;
-  const redirect_uri = '/auth/git';
-  const githubAuthUrl = `https://github.com/login/oauth/authorize?client_id=${client_id}&redirect_uri=${redirect_uri}`;
+app.get('/auth/git', (req, res) => {
+  const clientId = process.env.CLIENT_ID;
+  const redirectUri = 'https://webware-a3-7443f86eaef6.herokuapp.com/auth/git/callback';
+  const githubAuthUrl = `https://github.com/login/oauth/authorize?client_id=${clientId}&redirect_uri=${redirectUri}`;
   res.redirect(githubAuthUrl);
 });
 
-app.get('/auth/git', async (req, res) => {
+app.get('/auth/git/callback', async (req, res) => {
+  const code = req.query.code;
+  if (!code) {
+    return res.status(400).send('Code is missing');
+  }
+
   try {
     const tokenResponse = await axios.post(
       'https://github.com/login/oauth/access_token',
       {
         client_id: process.env.CLIENT_ID,
         client_secret: process.env.CLIENT_SECRET,
-        code: req.query.code,
+        code: code,
       },
       {
         headers: {
@@ -83,29 +88,26 @@ app.get('/auth/git', async (req, res) => {
       }
     );
 
-    const access_token = tokenResponse.data.access_token;
-    if (!access_token) {
-      return res.status(500).send('Failed to obtain access token.');
+    const accessToken = tokenResponse.data.access_token;
+    if (!accessToken) {
+      return res.status(400).send('Failed to obtain access token');
     }
 
-    const octokit = new Octokit({ auth: `token ${access_token}` });
-    const response = await octokit.users.getAuthenticated();
-    const user = response.data.login;
+    const userResponse = await axios.get('https://api.github.com/user', {
+      headers: {
+        Authorization: `token ${accessToken}`,
+      },
+    });
 
+    const user = userResponse.data;
     req.session.user = user;
-    req.session.user = user_login;
-    res.cookie('access_token', access_token, { httpOnly: true, secure: true });
-    res.cookie('user', user, { httpOnly: true, secure: true });
-
-    console.log(user);
-    res.redirect('/');
+    res.cookie('user', user.login, { maxAge: 900000, httpOnly: true });
+    res.redirect('/main.html');
   } catch (error) {
-    console.error('Error:', error.message);
-    if (error.response) {
-      console.error('Error details:', error.response.data);
-    }
-    return res.status(500).send('Internal Server Error');
+    console.error('Error during GitHub OAuth:', error);
+    res.status(500).send('Authentication failed');
   }
+
 });
 
 app.get('/posts', async (req, res) => {
